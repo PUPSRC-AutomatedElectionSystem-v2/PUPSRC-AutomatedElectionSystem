c509f3c1f8b587b128e026cdfa50fe65639d93a0
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-07-22 00:50:17 +0800

    Added an OR condition for pending setup account status.

--- src/includes/classes/voter-login-class.php
+++ src/includes/classes/voter-login-class.php
@@ -90,7 +90,7 @@ class Login extends IpAddress {
         $this->ip_manager->deleteIpAddress($this->ip_address);
 
         switch ($row['account_status']) {
-            case 'for_verification':
+            case 'for_verification' || 'pending_setup':
                 $this->redirectWithMessage($this->info_message, 'This account is under verification.');
                 break;
             case 'invalid':

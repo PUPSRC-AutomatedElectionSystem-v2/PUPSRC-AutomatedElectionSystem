d6ad3ba90accba93738508dedae4297bae2e871d
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-07-23 21:48:00 +0800

    chore: AT7-141-Drafted the email content for account setup.

--- src/includes/classes/email-sender.php
+++ src/includes/classes/email-sender.php
@@ -141,18 +141,31 @@ class EmailSender
     }
 
 
-    public function sendVerificationToken($recipientEmail, $verificationToken) {
+    public function sendVerificationToken($recipientEmail, $verificationToken, $orgName) 
+    {
         $subject = 'iVOTE Account Setup';
+        $resetPasswordLink = "http://localhost/PUPSRC-AutomatedElectionSystem/src/reset-password.php?token=$token&orgName=$orgName";
         $mailBody = <<<EOT
         <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>iVOTE Password Reset Request</title>
+            <title>$subject</title>
         </head>
         <body">
-            <p>Verification Token: $verificationToken</p>
+            <p>Dear Iskolar,</p>
+            <p>We have received a request to setup your account. To complete the process, 
+            please follow the instructions below:</p>
+            <ul style="padding-left: 20px;">
+                <li>Click on the following link to setup your account: <a href="$resetPasswordLink">Reset Password Link</a></li>
+                <li>The password reset link is only available for 30 minutes.</li>
+                <li>If you are unable to click the link above, please copy and paste it into your browser's address bar.</li>
+                <li>Once the link opens, you will be prompted to enter a new password for your account. Please choose a strong and secure password to ensure the safety of your account.</li>
+            </ul>
+            <p>If you did not initiate this, please disregard this email.</p>
+            <p>Thank you for your attention to this matter.</p>
+            <p>Best regards,<br>iVOTE Team</p>
         </body>
         </html>
         EOT;

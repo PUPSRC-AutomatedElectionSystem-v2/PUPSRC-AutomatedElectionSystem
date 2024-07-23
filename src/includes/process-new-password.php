21bf25756b1f2a2039368e75997dfbf454c881ba
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-07-23 21:46:15 +0800

    chore: AT7-141-Created a new file to process the new password from account setup.

---
+++ src/includes/process-new-password.php
@@ -0,0 +1,95 @@
+<?php
+include_once str_replace('/', DIRECTORY_SEPARATOR, __DIR__ . '/classes/file-utils.php');
+require_once FileUtils::normalizeFilePath(__DIR__ . '/session-handler.php');
+require_once FileUtils::normalizeFilePath(__DIR__ . '/classes/db-connector.php');
+require_once FileUtils::normalizeFilePath(__DIR__ . '/classes/db-config.php');
+include_once FileUtils::normalizeFilePath(__DIR__ . '/error-reporting.php');
+include_once FileUtils::normalizeFilePath(__DIR__ . '/default-time-zone.php');
+
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $token = $_POST['token'];
+    $password = trim($_POST['password']);
+    $password_confirmation = trim($_POST['password_confirmation']);
+    $token_hash = hash("sha256", $token);
+
+    $error = newPasswordValidation($password);
+
+    if ($error) {
+        setErrorAndRedirect($error, $token, $org_name);
+    }
+
+    if ($password !== $password_confirmation) {
+        setErrorAndRedirect('Your passwords do not match.', $token, $org_name);
+    }
+
+    $connection = DatabaseConnection::connect();
+
+    $sql = "SELECT * FROM voter WHERE reset_token_hash = ?";
+    $stmt = $connection->prepare($sql);
+    $stmt->bind_param("s", $token_hash);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+
+    if (!$row) {
+        setErrorAndRedirect('Your password reset link was not found.', NULL, NULL, '../voter-login.php');
+    }
+
+    $expiry_time = strtotime($row["reset_token_expires_at"]);
+    $current_time = time();
+
+    if ($expiry_time <= $current_time) {
+        setErrorAndRedirect('Your password reset link has expired.', NULL, NULL, '../voter-login.php');
+    }
+
+    $new_password = password_hash($password_confirmation, PASSWORD_DEFAULT);
+
+    $success = updatePassword($connection, $new_password, $row['email']);
+
+    if($_SESSION['organization'] !== 'sco') {
+        $sco = 'sco';
+        $config = DatabaseConfig::getOrganizationDBConfig($sco);
+        $sco_connection = new mysqli($config['host'], $config['username'], $config['password'], $config['database']);
+
+        $success &= updatePassword($sco_connection, $new_password, $row['email']);        
+    }
+
+    if ($success) {
+        echo json_encode(['success' => true]);
+    } 
+    else {
+        setErrorAndRedirect("Failed to reset your password. Please try again.", $token, $org_name);
+    }
+
+    exit();
+}
+
+// Function to validate new password
+function newPasswordValidation($password) {
+    $password_regex = '/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[\W_])(?=.*[^\s]).{8,20}$/';
+
+    if (!preg_match($password_regex, $password)) {
+        return "Password must be 8-20 characters with a number, an uppercase letter, a lowercase letter, a special character, and with no whitespace/s.";
+    }
+    return "";
+}
+
+
+function updatePassword($connection, $new_password, $email) {
+    $verified_status = 'verified';
+    $sql = "UPDATE voter SET password = ?, account_status = ? reset_token_hash = NULL, reset_token_expires_at = NULL WHERE BINARY email = ?";
+    $stmt = $connection->prepare($sql);
+    $stmt->bind_param('sss', $new_password, $verified_status, $email);
+    return $stmt->execute();
+}
+
+
+function setErrorAndRedirect($message, $token, $org_name, $redirect_url = '../reset-password.php') {
+    $_SESSION['error_message'] = $message;
+    if ($token) {
+        $redirect_url .= "?token=" . urlencode($token) . "&orgName=" . urlencode($org_name);
+    }
+    header("Location: $redirect_url");
+    exit();
+}

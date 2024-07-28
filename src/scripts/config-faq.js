9066e3aa7a1f74bfc517caca749a2a76dc2eeab5
___Sundae <jennymaelbaldoza@iskolarngbayan.pup.edu.ph>
2024-07-29 00:12:33 +0800

    chore: additional improvements on darkmode styles on tables row reordering

--- src/scripts/config-faq.js
+++ src/scripts/config-faq.js
@@ -1120,11 +1120,8 @@ ConfigPage.EditorModal = class {
         { className: 'ql-italic', icon: 'italic' },
         { className: 'ql-underline', icon: 'underline' },
         {
-            className: 'ql-list', value: 'bullet', svg: `<svg viewBox="0 0 18 18">
-                <line class="ql-stroke" x1="6" x2="15" y1="4" y2="4"></line><line class="ql-stroke" x1="6" x2="15" y1="9" y2="9"></line>
-                <line class="ql-stroke" x1="6" x2="15" y1="14" y2="14"></line><line class="ql-stroke" x1="3" x2="3" y1="4" y2="4"></line>
-                <line class="ql-stroke" x1="3" x2="3" y1="9" y2="9"></line><line class="ql-stroke" x1="3" x2="3" y1="14" y2="14"></line>
-            </svg>` },
+            className: 'ql-list', value: 'bullet', svg: `<svg viewBox="0 0 18 18"><line class="ql-stroke" x1="6" x2="15" y1="4" y2="4"></line><line class="ql-stroke" x1="6" x2="15" y1="9" y2="9"></line><line class="ql-stroke" x1="6" x2="15" y1="14" y2="14"></line><line class="ql-stroke" x1="3" x2="3" y1="4" y2="4"></line><line class="ql-stroke" x1="3" x2="3" y1="9" y2="9"></line><line class="ql-stroke" x1="3" x2="3" y1="14" y2="14"></line></svg>`
+        },
         {
             className: 'ql-list', value: 'ordered', svg: `<svg viewBox="0 0 18 18">
                 <line class="ql-stroke" x1="7" x2="15" y1="4" y2="4"></line>

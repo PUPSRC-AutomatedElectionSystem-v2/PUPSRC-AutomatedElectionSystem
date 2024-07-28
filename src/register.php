5a014a54cd583a47070feb4d673408c5c2ccecb2
Biella Mariscotes <biella.requina@gmail.com>
2024-07-28 22:42:52 +0800

    style: added note regarding the disabled course options

--- src/register.php
+++ src/register.php
@@ -94,7 +94,7 @@ $registration_success = isset($_SESSION['registration_success']) && $_SESSION['r
     include_once FileUtils::normalizeFilePath(__DIR__ . '/includes/components/outside-header.php');
     ?>
 
-    <div class="register-all-container pt-3 pb-5" style="padding-top: 0.8rem">
+    <div class="register-all-container pt-3" style="padding-top: 0.8rem">
         <div class="row mt-5 pl-5 pb-4 container-fluid">
             <div class="col-md-6 slide-in main-reg-container">
                 <form id="register-form" action="includes/registration-inc.php" method="POST"
@@ -239,7 +239,11 @@ $registration_success = isset($_SESSION['registration_success']) && $_SESSION['r
                                         <option value="jpia">JPIA</option>
                                         <option value="piie">PIIE</option>
                                     </select>
+
+                                    <div class="form-text mt-2" style="font-size: 11px;"><span
+                                            class="main-blue fw-semibold">Note:</span> Disabled options indicate that registration is currently unavailable.</div>
                                 </div>
+                                
                             </div>
                         </div>
 
